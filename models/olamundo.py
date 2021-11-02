f23592471bea1a20788d3e26d25701d7133e2069
rulonunhez <rulonunhez@gmail.com>
2021-11-02 08:45:46 +0100

    Primer commit

---
+++ models/olamundo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class olamundo(models.Model):
+     _name = 'odoo_olamundo.olamundo'
+     _description = 'Exemplo de olamundo'
+
+     name = fields.Char(string = 'Ola Mundo')
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
